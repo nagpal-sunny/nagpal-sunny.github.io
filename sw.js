9928bcf1c39994320914a1b40bd66ec0557702f7
Saurav Nagpal <saurav.nagpal@nagarro.com>
2023-07-18 11:47:08 +0530

    Add requirement issue.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230718.110059';
+const cacheName = 'chirpy-20230718.114357';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
