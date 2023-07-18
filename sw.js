7cc2385c2ee72709b0273768486b5b87c6e13206
Saurav Nagpal <saurav.nagpal@nagarro.com>
2023-07-18 11:02:30 +0530

    Nil coalescing.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230717.205920';
+const cacheName = 'chirpy-20230718.110059';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
