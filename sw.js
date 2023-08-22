a66d9f16433305a6e60365cef9b0f531f4f36690
Saurav Nagpal <saurav.nagpal@nagarro.com>
2023-08-22 11:15:09 +0530

    Fix the issue.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230821.220227';
+const cacheName = 'chirpy-20230822.111247';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
