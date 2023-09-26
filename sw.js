ef2e9ea535a7ef7b876cbb83ba9384342d05efb1
Saurav Nagpal <saurav.nagpal@nagarro.com>
2023-09-26 16:26:17 +0530

    Upload the config blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230926.152729';
+const cacheName = 'chirpy-20230926.161340';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
