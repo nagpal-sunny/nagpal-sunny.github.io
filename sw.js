1fd8c3f77e92e781ef7f3d96aa0809684947c98d
Saurav Nagpal <nagpalsunny15@gmail.com>
2023-12-19 11:11:40 +0530

    Week 2 shorts.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231212.153149';
+const cacheName = 'chirpy-20231219.110902';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
